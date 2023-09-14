ccfef8c5c1196c0aa838114be66f220fd90317d3
Salmamjg <salmamejgarii2019@gmail.com>
2023-09-14 07:38:39 -0700

    Updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,16 @@
 
 void print_line(int n)
 {
-int chr;
-if(n <= 0)
-_putchar('\n');
-else
-{
-for (chr = 1; chr <= n; chr++)
-{
-_putchar('_');
-}
-}
-_putchar('\n');
+    int chr;
+    
+    if (n <= 0)
+        _putchar('\n');
+    else
+    {
+        for (chr = 1; chr <= n; chr++)
+        {
+            _putchar('_');
+        }
+        _putchar('\n');
+    }
 }
