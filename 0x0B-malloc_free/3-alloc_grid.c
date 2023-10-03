2b2d82a076ae3875c2ad12812ae2d099b7740ac0
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-03 21:31:39 +0100

    git commit -m "Updated"

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
+
 int **alloc_grid(int width, int height)
 {
 	int **grid;
