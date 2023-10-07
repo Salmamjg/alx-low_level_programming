baff98175d83c3f83dfb1a0bdc0680b3e306eaec
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-07 21:49:48 +0100

    git commit -m "Updated"

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,43 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *t;
-	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
+    char *t;
+    unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-	while (s1 != NULL && s1[length1] != '\0') 
-		length1++;
-	while (s2 != NULL && s2[length2] != '\0') 
-		length2++;
+    while (s1 != NULL && s1[length1] != '\0')
+        length1++;
+    while (s2 != NULL && s2[length2] != '\0')
+        length2++;
 
-	if (n < length2)
-	   t = malloc(sizeof(char) * (length1 + n + 1));
-	else
-	   t = malloc(sizeof(char) * (length1 + length2 + 1));
+    if (n < length2)
+        t = malloc(sizeof(char) * (length1 + n + 1));
+    else
+        t = malloc(sizeof(char) * (length1 + length2 + 1));
 
-	if (!t)
-		return (NULL);
+    if (!t)
+        return (NULL);
 
-	while (i < length1)
-	{
-		t[i] = s1[i];
-		i++;
-	}
+    while (i < length1)
+    {
+        t[i] = s1[i];
+        i++;
+    }
 
-	while (n < length2 && i < (length1 + n))
-		t[i] = s2[j];
+    while (n < length2 && i < (length1 + n))
+    {
+        t[i] = s2[j];
         i++;
         j++;
+    }
 
-	while (n >= length2 && i < (length1 + length2))
-		t[i] = s2[j];
+    while (n >= length2 && i < (length1 + length2))
+    {
+        t[i] = s2[j];
         i++;
         j++;
+    }
 
-	t[i] = '\0';
+    t[i] = '\0';
 
-	return (t);
+    return (t);
 }
