261f596a86fd0a7880e856f7b74f91ad197c6501
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-08 11:14:14 +0100

    Updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,41 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *t;
-    unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
+	char *t;
+	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	while (s1 && s1[length1])
+		length1++;
+	while (s2 && s2[length2])
+		length2++;
 
-    while (s1[length1] != '\0')
-        length1++;
-    while (s2[length2] != '\0')
-        length2++;
+	if (n < length2)
+		t = malloc(sizeof(char) * (length1 + n + 1));
+	else
+		t = malloc(sizeof(char) * (length1 + length2 + 1));
 
-    if (n >= length2)
-        n = length2;
+	if (!t)
+		return (NULL);
 
-    t = malloc(sizeof(char) * (length1 + n + 1));
+	while (i < length1)
+	{
+		t[i] = s1[i];
+		i++;
+	}
 
-    if (!t)
-        return (NULL);
+	while (n < length2 && i < (length1 + n))
+		t[i++] = s2[j++];
 
-    while (i < length1)
-    {
-        t[i] = s1[i];
-        i++;
-    }
+	while (n >= length2 && i < (length1 + length2))
+		t[i++] = s2[j++];
 
-    while (j < n)
-    {
-        t[i] = s2[j];
-        i++;
-        j++;
-    }
+	t[i] = '\0';
 
-    t[i] = '\0';
-
-    return (t);
+	return (t);
 }
