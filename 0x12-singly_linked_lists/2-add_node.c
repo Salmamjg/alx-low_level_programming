68b49dd7bee57842292c2afee1d886ee6d9f8349
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-19 23:32:13 +0100

    Updated & fixed errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of the linked list list_t
+ * add_node - adds a new node at the beginning of the linked list 
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
  *
