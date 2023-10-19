b8dc4db0834e16e13260ba49d74bea4f0ee6630c
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-19 21:03:24 +0100

    Updated & efix errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /*
- *print_list : prints the elements of list_t linked list
- *@h : pointer to the linked list list_t that we are going to print
- *
- *Return : the number of nodes
- */
+*print_list : prints the elements of list_t linked list
+*@h : pointer to the linked list list_t that we are going to print
+*
+*Return : the number of nodes
+*/
 
 size_t print_list(const list_t *h){
 size_t count = 0;
