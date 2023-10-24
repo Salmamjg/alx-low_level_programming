5d2065e29dad96d20ec12228d0545dd4d592f870
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-24 20:02:50 +0100

    Updated & fixed errors

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,20 +4,20 @@
  * add_nodeint - adds a new node at the beginning of the listint_t list
  * @head: double pointer to the listint_t list
  * @n:integer value to add to the new node
- * 
+ *
  * Return: the address of the new element, or NULL if it fails
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *updated_node = malloc(sizeof(listint_t));
 
-if(updated_node == NULL)
+if (updated_node == NULL)
 {
-return NULL;
+return (NULL) ;
 }
 updated_node->n = n;
 updated_node->next = *head;
 *head = updated_node;
 
-return updated_node;
+return (updated_node);
 }
