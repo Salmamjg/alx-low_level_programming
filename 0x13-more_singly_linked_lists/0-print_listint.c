3107eedec2e6d38e66aaa57b84f6027941fb5ff2
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-24 19:03:44 +0100

    Updated & fixed errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,18 +8,19 @@
 * Return: The number of nodes
 */
 
-size_t print_listint(const listint_t *h){
-    size_t c = 0;
-    
-    if (h==NULL) 
-         {
-            return 0;
-         }     
+size_t print_listint(const listint_t *h)
+{
+ size_t c = 0;    
+    if (h == NULL) 
+        {
+            return(0);
+        }     
 
-    while (h!=NULL){
-     printf("%d\n", h->n);
-     h = h ->next;
-     c++;
+    while (h!=NULL)
+    {
+    printf("%d\n", h->n);
+    h = h ->next;
+    c++;
     }
-return c;
+return (c);
 }
