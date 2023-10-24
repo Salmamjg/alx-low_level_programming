0fdcc5a8b56699fa035e705c3ee45e73746ac05f
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-24 19:13:50 +0100

    Updated & fixed errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,17 +10,16 @@
 
 size_t print_listint(const listint_t *h)
 {
-    size_t c = 0;    
+size_t c = 0;    
 if (h == NULL) 
 {
-    return (0);
+return (0);
 }     
-
 while (h != NULL)
 {
-    printf("%d\n", h->n);
-    h = h->next;
-    c++;
+printf("%d\n", h->n);
+h = h->next;
+c++;
 }
 return (c);
 }
