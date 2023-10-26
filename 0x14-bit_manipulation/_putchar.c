c9be92a911fba2af04b103d950ddd000acd3c1d9
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-26 19:55:45 +0100

    Updated & fixed betty style

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -4,10 +4,10 @@
  * _putchar - writes the character c to stdout.
  * @c: The character to be written
  *
- * Return: 1 if it succeeded, 0 otherwise. 
+ * Return: 1 if it succeeded, 0 otherwise.
 */
 
 int _putchar(char c)
 {
-return write(1, &c, 1);
+return (write(1, &c, 1));
 }
