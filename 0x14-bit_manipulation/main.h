0a75166d799e2fd366b7a801d240ff6f6abdf6e8
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-26 17:15:42 +0100

    Updated & fixed betty style

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,17 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * main.h - Header file containing function description for all fucntion used in this directory
+*/
+
 unsigned int binary_to_uint(const char *b);
-
-
-
-
-
-
-
-
-
-
 
 
 
