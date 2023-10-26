b7565e981a6e97a3078ec73367b90895f067b569
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-26 18:16:22 +0100

    Updated & fixed errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,7 +5,7 @@
  * print_binary - Prints the binary reprensation of a number without % or /.
  * @n: The unsigned long integer to the printed in binary.
  *
- * Return: No return value . 
+ * Return: No return value.
 */
 
 void print_binary(unsigned long int n)
@@ -17,13 +17,13 @@ if (n == 0)
 _putchar('0');
 return;
 }
-for(i=s-1; i>= 0; i--)
+for (i = s-1; i >= 0; i -- )
 {
 if ((n >> i) & 1)
 {
 _putchar('1');
 }
-else 
+else
 {
 putchar('0');
 }
