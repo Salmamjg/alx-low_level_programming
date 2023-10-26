d964be2790d0929c2aa92b3bd9d14e803293e099
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-26 22:18:53 +0100

    Updated & fixed betty style error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,7 @@
  * flip_bits - returns the number of bits flip from one number to another.
  * @n: the first unsigned long integer.
  * @m: the second unsigned long integer.
- * 
+ *
  * Return: the number of bits to flip
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
@@ -17,5 +17,5 @@ while (x > 0)
 y += x & 1;
 x >>= 1;
 }
-return y;
+return (y);
 }
