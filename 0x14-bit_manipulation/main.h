abc7e02db640c84e85f879c799bfe3824ce9ee17
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-26 17:27:56 +0100

    Updated & fixed errors

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/**
- * main.h - Header file containing function description for all fucntion used in this directory
-*/
+#include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
 
