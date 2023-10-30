810abf90f69d322d02d6d9230bad6094dd2a789b
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-30 23:13:32 +0100

    Updated and fixed errors

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_I/O
+#define FILE_I/O
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -5,3 +8,5 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+
+#endif
