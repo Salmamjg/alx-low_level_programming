ec1e7bdc824a429adf6a3c54e1862794680be0b5
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-30 22:56:05 +0100

    Updated & fixed errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,15 +38,21 @@ return (0);
 }
 
 b_read = read(f_desc, table, letters);
+if (b_read == -1)
+{
+free(table);
+close(f_desc);
+return (0);
+}
 b_written = write(STDOUT_FILENO, table, b_read);
 
 close(f_desc);
 free(table);
 
-
-if (b_read == -1 || b_written == -1 || b_written != b_read)
+if (b_written == -1 || b_written != b_read)
 {
 return (0);
 }
+
 return (b_read);
 }
