cfe0ffc76750901b4d0d59330ddf162b283db07e
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-30 22:14:04 +0100

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,56 +8,54 @@
  * @filename: the name of the file to read.
  * @letters: the number of letters to read and print.
  * 
- *Return: the actual number of letters read and printed, or 0 if it failed. 
-*/
-
+ * Return: the actual number of letters read and printed, or 0 if it failed. 
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+    int f_desc;
+    char *table = malloc(letters);
+    ssize_t b_read = 0;
+    ssize_t b_written;
+    char c;
+    ssize_t total_chars = 0;
 
-int f_desc = open(filename, O_RDONLY);
-char *table;
-ssize_t b_read ;
-ssize_t b_written ;
+    if (filename == NULL || table == NULL)
+    {
+        if (table != NULL)
+            free(table);
+        return (0);
+    }
 
-if (filename == NULL)
-{
-return (0);
-}
+    f_desc = open(filename, O_RDONLY);
 
-f_desc = open(filename, O_RDONLY);
+    if (f_desc == -1)
+    {
+        free(table);
+        return (0);
+    }
 
-if (f_desc == -1)
-{
-return (0);
-}
+    while ((b_read = read(f_desc, &c, 1)) > 0)
+    {
+        if (b_read == -1)
+        {
+            free(table);
+            close(f_desc);
+            return (0);
+        }
 
-table = (char *)malloc(letters);
-if(table == NULL)
-{
-close(f_desc);
-return (0);
-}
+        b_written = write(STDOUT_FILENO, &c, 1);
 
-b_read = read(f_desc, table, letters);
+        if (b_written == -1)
+        {
+            free(table);
+            close(f_desc);
+            return (0);
+        }
 
-if (b_read == -1)
-{
-close(f_desc);
-free(table);
-return (0);
-}
+        total_chars++;
+    }
 
-b_written = write(STDOUT_FILENO, table, b_read);
-
-if (b_written == -1 || b_written != b_read)
-{
-close(f_desc);
-free(table);
-return (0);
-}
-
-close(f_desc);
-free(table);
-return (b_read);
-
+    free(table);
+    close(f_desc);
+    return total_chars;
 }
