89845c7e07eff6f6de98abe26964e46cc63e7c4e
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-30 23:25:54 +0100

    Updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -5,3 +7,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+
+
+#endif
