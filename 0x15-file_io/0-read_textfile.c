55379ba9fb6b88f046bc61bce7ae342d9791b1e5
Salmamjg <salmamejgarii2019@gmail.com>
2023-10-31 17:53:47 +0100

    Updated & fixed betty style errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * read_textfile - function that reads and prints the content of a file.
  * @filename: the name of the file to read.
  * @letters: the number of letters to read and print.
+ *
  *Return: the actual number of letters read and printed, or 0 if it failed.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int f_desc = open(filename, O_RDONLY);
-if(!filename)
+int f_desc;
+char *table = malloc(sizeof(char) * (letters));
+ssize_t b_r;
+ssize_t b_w;
+f_desc = open(filename, O_RDONLY);
+if (f_desc == -1)
 return (0);
-if(f_desc == -1)
-return (0);
-char *table = malloc(letters);
-if (!table)
-{
-close(f_desc);
-return (0);
-}
-ssize_t b_read = read(f_desc, table, letters);
-close(f_desc);
-if (b_read == -1)
-{
-free (table);
-return (0);
-}
-ssize_t b_write = write(STDOUT_FILENO, table, b_read);
+table = malloc(sizeof(char) * letters);
+b_r = read(f_desc, table, letters);
+b_w = write(STDOUT_FILENO, table, b_r);
 free(table);
-return (b_write == -1 || b_write != b_read ? 0 : b_read);
+close(f_desc);
+return (b_w);
 }
