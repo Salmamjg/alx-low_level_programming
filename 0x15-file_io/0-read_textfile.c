7411c2fed583441260e4032ca2aef4b58f315429
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-07 15:43:45 +0100

    Updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,15 +14,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int f_desc;
 char *table = malloc(sizeof(char) * (letters));
-ssize_t b_r;
-ssize_t b_w;
+ssize_t b_read;
+ssize_t b_written;
+if (filename == NULL)
+return (0);
 f_desc = open(filename, O_RDONLY);
 if (f_desc == -1)
+{
+free(table);
 return (0);
-table = malloc(sizeof(char) * letters);
-b_r = read(f_desc, table, letters);
-b_w = write(STDOUT_FILENO, table, b_r);
+}
+if (!table)
+{
+return (0);
+}
+b_read = read(f_desc, table, letters);
+if (b_read == -1)
+{
 free(table);
 close(f_desc);
-return (b_w);
+return (0);
+}
+b_written = write(STDOUT_FILENO, table, b_read);
+
+close(f_desc);
+free(table);
+
+if (b_written == -1 || b_written != b_read)
+{
+return (0);
+}
+
+return (b_read);
 }
---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <elf.h>
+
+void display_magic(unsigned char e_ident[ET_NIDENT])
+{
+int i = 0;
+printf("Magic: ");
+while(i < EI_NIDENT)
+{
+
+}
+}
