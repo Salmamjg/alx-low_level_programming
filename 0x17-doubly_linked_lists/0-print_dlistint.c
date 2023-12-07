c1a7affcb53af6df4c36ac0d14e4b02347635b53
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-07 16:02:30 +0100

    Updated

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
 /**
  * print_dlistint - Prints all elements of a dlistint_t list
  * @h: Pointer to the head of the doubly linked list
- * 
+ *
  * Return: The number of nodes in the list
 */
 size_t print_dlistint(const dlistint_t *h)
